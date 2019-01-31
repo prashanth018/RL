6f290b45f1cbb64289320073cfd33158a1271f39
prashanth018 <prashanthsedan@gmail.com>
2019-01-31 21:42:56 +0530

    Added method for value iteration over the GridWorld Environment

--- PolicyEvaluation.py
+++ PolicyEvaluation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import copy
 
 if "../" not in sys.path:
     sys.path.append("../")
@@ -45,10 +46,10 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
     return np.array(V)
 
 
-# Tabular Policy Improvement
-def policy_improvement(env, discount_factor=1.0):
+# Tabular Policy Iteration
+def policy_iteration(env, discount_factor=1.0):
     """
-    Policy Improvement Algorithm. Iteratively evaluates and improves a policy
+    Policy Iteration Algorithm. Iteratively evaluates and improves a policy
     until an optimal policy is found.
 
     Args:
@@ -69,6 +70,15 @@ def policy_improvement(env, discount_factor=1.0):
         # Giving the current policy and getting it evaluated.
         V = policy_eval(policy, env, discount_factor)
         optimal = True
+
+        # Uncomment the below code to see how the value and policy changes over iterations.
+        # print("Reshaped Grid Value Function:")
+        # print(V.reshape(env.shape))
+        # print("")
+        # print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+        # print(np.reshape(np.argmax(policy, axis=1), env.shape))
+        # print("***********")
+
 
         for states in range(env.nS):
             # Since this is a model based environment, greedy approach
@@ -92,16 +102,68 @@ def policy_improvement(env, discount_factor=1.0):
         if optimal:
             return policy, V
 
+# Uncomment below code to see the output of policy iteration.
+# policy, v = policy_iteration(env)
+# print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+# print(np.reshape(np.argmax(policy, axis=1), env.shape))
+# print("")
+#
+# print("Reshaped Grid Value Function:")
+# print(v.reshape(env.shape))
+# print("")
 
-policy, v = policy_improvement(env)
-print("Policy Probability Distribution:")
-print(policy)
-print("")
 
-print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
-print(np.reshape(np.argmax(policy, axis=1), env.shape))
-print("")
+# Tabular Value Iteration
+def value_iteration(env, theta=0.0001, discount_factor=1.0):
+    """
+    Value Iteration Algorithm.
 
-print("Reshaped Grid Value Function:")
-print(v.reshape(env.shape))
-print("")
+    Args:
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment.
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+
+    Returns:
+        A tuple (policy, V) of the optimal policy and the optimal value function.
+    """
+
+    V = np.zeros(env.nS)
+    policy = np.ones([env.nS, env.nA]) / env.nA
+    while True:
+        prev_policy = copy.deepcopy(policy)
+
+        # Uncomment the below code to see how the value and policy changes over iterations.
+        # print("Reshaped Grid Value Function:")
+        # print(V.reshape(env.shape))
+        # print("")
+        # print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+        # print(np.reshape(np.argmax(policy, axis=1), env.shape))
+        # print("***********")
+
+        for states in range(env.nS):
+            action_Array = np.zeros(env.nA)
+            for actions in range(env.nA):
+                action_val = 0
+                for prob, next_state, reward, done in env.P[states][actions]:
+                    action_val += prob * (reward + discount_factor * V[next_state])
+                action_Array[actions] = action_val
+            V[states] = np.max(action_Array)
+            policy[states] = np.eye(env.nA)[np.argmax(action_Array)]
+            # print(action_Array)
+        if np.all(prev_policy == policy):
+            return policy, V
+
+
+
+# Uncomment below code to see the output of value iteration.
+# policy,V = value_iteration(env)
+# print("Reshaped Grid Value Function:")
+# print(V.reshape(env.shape))
+# print("")
+#
+# print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+# print(np.reshape(np.argmax(policy, axis=1), env.shape))
+# print("")
