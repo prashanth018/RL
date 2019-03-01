f3912860a5af8a67d19b529bc6121e82c97294ba
prashanth018 <prashanthsedan@gmail.com>
2019-03-01 23:46:48 +0530

    added a method which does SARSA on Windy Grid World environment.

--- tabular/WindyGridWorld.py
+++ tabular/WindyGridWorld.py
@@ -1,18 +1,50 @@
 import gym
 import numpy as np
 import sys
+from collections import defaultdict
 
 if "../" not in sys.path:
-  sys.path.append("../")
+    sys.path.append("../")
 
-from libs.envs.windy_gridworld import WindyGridworldEnv
-
-env = WindyGridworldEnv()
-
-print(env.reset())
-env.render()
+from lib.envs.windy_gridworld import WindyGridworldEnv
 
 
-for i in range(8):
-    print(env.step(1))
-    env.render()
+def fn_policy(env, Q, epsilon=0.05):
+    nA = env.nA
+    nS = env.nS
+    policy = np.ones((nS, nA)) * epsilon / nA
+    for state in range(nS):
+        action_array = Q[state]
+        policy[np.argmax(action_array)] += (1 - epsilon)
+    return policy
+
+
+def sarsa_WindyGridWorld(Q, env, discount_factor=1.0, alpha=0.05, epsilon=0.05):
+    episodes = 200
+    nS = env.nS
+    nA = env.nA
+    for ep in range(episodes):
+        timesteps = 200
+        current_state = env.reset()
+        policy = fn_policy(env, Q)
+        for ts in range(timesteps):
+            action_arr = policy[current_state]
+            action = np.random.choice(np.arange(action_arr), p=action_arr)
+            next_state, reward, done, prob = env.step(action)
+            next_action_arr = policy[next_state]
+            next_action = np.random.choice(np.arange(next_action_arr), p=next_action_arr)
+            Q[current_state][action] = Q[current_state][action] + alpha * (
+                    reward + (discount_factor * Q[next_state][next_action]) - Q[current_state][action])
+            act_arr = np.ones((nS, nA)) * epsilon / nA
+            act_arr[np.argmax(Q[current_state])] += (1-epsilon)
+            policy[current_state] = act_arr
+
+
+if __name__ == "__main__":
+    # for i in range(15):
+    #     print(env.step(1))
+    #     env.render()
+
+    env = WindyGridworldEnv()
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    sarsa_WindyGridWorld(Q, env)
