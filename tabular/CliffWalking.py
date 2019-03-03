53359d84810e54585b6e2545f42977f4fa7029cd
prashanth018 <prashanthsedan@gmail.com>
2019-03-03 16:24:29 +0530

    Experimenting with the cliff walking environment.

---
+++ tabular/CliffWalking.py
@@ -0,0 +1,22 @@
+import gym
+import numpy as np
+import sys
+
+if "../" not in sys.path:
+    sys.path.append("../")
+
+from lib.envs.cliff_walking import CliffWalkingEnv
+
+env = CliffWalkingEnv()
+
+if __name__ == "__main__":
+    print(env.reset())
+    env.render()
+    print(env.step(1))
+    env.render()
+    print(env.step(2))
+    env.render()
+    print(env.step(0))
+    env.render()
+    print(env.step(3))
+    env.render()
